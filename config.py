19797ce4d7729884ad492cc281739d482b09020e
Thapwarit <89914408+thapwaritp@users.noreply.github.com>
2023-11-07 22:50:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,23 @@ def banner():
     os.system("figlet -f big VERUS")
     os.system("figlet -f digital server version 1.1")
     print("\033[00m\n")
-    print("\033[96mEdit by PICHET SAENGTEWAN\033[0m")
+    print("\033[96mEdit by THAPWARIT PRUEKTRANON\033[0m")
+    print("\033[96mCredit by PICHET SAENGTEWAN\033[0m")
     print("\033[96mDebug Colors by AMARIN PRAPRIWIT\033[0m")
     print("\033[36m\033[0m")
     
-    print(f"\n\033[91mกำลัง Run Mining\033[0m\n")
+    print(f"\n\033[91m   Run Mining...\033[0m\n")
 
 def bannerup():
     os.system("clear")
     print("\033[1;34;34m")
     os.system("figlet -f big UPDATE")
     print("\033[00m\n")
-    print(f"\n\033[96mกำลัง UPDATE Miner\033[0m\n")
+    print(f"\n\033[96m   UPDATE Miner...\033[0m\n")
 
 def banneredit():
     os.system("clear")
     print("\033[1;34;34m")
     os.system("figlet -f big [-EDIT-]")
     print("\033[00m\n")
-    print(f"\n\033[96mEdit by PICHET SAENGTEWAN\033[0m\n")
+    print(f"\n\033[96mEdit by THAPWARIT  PRUEKTRANON\033[0m\n")
